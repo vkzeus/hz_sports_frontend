3481efae890385995984cbe1c3775e1356179ff2
Vedant Kajale <kajalevedant7@gmail.com>
2025-11-08 19:38:05 +0530

    urlChange

--- src/pages/login/api/loginApi.jsx
+++ src/pages/login/api/loginApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000"; // adjust if deployed
+const API_URL = "https://hz-sports-backend-2.onrender.com"; // adjust if deployed
 
 export const loginApi = (data) => axios.post(`${API_URL}/api/auth/login`, data);
 export const registerApi = (data) => axios.post(`${API_URL}/api/auth/register`, data);

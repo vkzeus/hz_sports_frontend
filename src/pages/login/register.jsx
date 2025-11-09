2c8bfb0c33053de1dd1c1212e6b7c66d80cff54e
Vedant Kajale <kajalevedant7@gmail.com>
2025-11-09 08:33:36 +0530

    registerPage

--- src/pages/login/register.jsx
+++ src/pages/login/register.jsx
@@ -10,34 +10,33 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { registerRequest } from "./slice/loginSlice";
 
 function Register() {
   const navigate = useNavigate();
+  const dispatch =useDispatch();
 
   const [formData, setFormData] = useState({
-    clientName: "",
-    firstName: "",
-    lastName: "",
-    clientType: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    phoneNumber: "",
-    accessLevel: "",
+     "role": "",
+    "name": "",
+    "email": "",
+    "class": "",
+    "sport": "",
+    "enrollmentId": "",
+    "password": ""
   });
 
   const handleChange = (field) => (event) => {
     setFormData({ ...formData, [field]: event.target.value });
   };
- const handleSubmit=async ()=>{
-//     try{
-// const res=await axios.post(registerUrl,formData,{loginHeader})
-//   if(res.hzStatus===200){
-//     navigate('/login',{replace:true})
-//   }
-//     }catch(error){
-//         console.log(error)
-//     }
+ const handleSubmit= (e)=>{
+  console.log(formData)
+e.preventDefault();
+const payload = {
+  ...formData
+}
+dispatch(registerRequest(payload))
  }
 
 
@@ -72,54 +71,56 @@ function Register() {
           }}
         >
           <TextField
-            label="Client Name"
+            label="Sport"
             variant="outlined"
             fullWidth
-            value={formData?.clientName || ""}
-            onChange={handleChange("clientName")}
+            value={formData?.sport || ""}
+            onChange={handleChange("sport")}
           />
           <TextField
             label="First Name"
             variant="outlined"
             fullWidth
-            value={formData?.firstName || ""}
-            onChange={handleChange("firstName")}
+            value={formData?.name || ""}
+            onChange={handleChange("name")}
           />
           <TextField
-            label="Last Name"
+            label="Email"
             variant="outlined"
+            type="email"
             fullWidth
-            value={formData?.lastName || ""}
-            onChange={handleChange("lastName")}
+            value={formData?.email || ""}
+            onChange={handleChange("email")}
           />
           <TextField
             label="Client Type"
             select
             variant="outlined"
             fullWidth
-            value={formData?.clientType || ""}
-            onChange={handleChange("clientType")}
+            value={formData?.role || ""}
+            onChange={handleChange("role")}
           >
-            <MenuItem value="operator">Operator</MenuItem>
-            <MenuItem value="advertiser">Advertiser</MenuItem>
+            <MenuItem value="Student">Student</MenuItem>
+            <MenuItem value="Coach">Coach</MenuItem>
+            <MenuItem value="Coach">Parent</MenuItem>
           </TextField>
-          <TextField
+          {/* <TextField
             label="Email"
             variant="outlined"
             fullWidth
             type="email"
             value={formData?.email || ""}
             onChange={handleChange("email")}
-          />
-          <TextField
+          /> */}
+          {/* <TextField
             label="Phone Number"
             variant="outlined"
             fullWidth
             type="tel"
             value={formData?.phoneNumber || ""}
             onChange={handleChange("phoneNumber")}
-          />
-          <TextField
+          /> */}
+          {/* <TextField
             label="Access Level"
             select
             variant="outlined"
@@ -129,7 +130,15 @@ function Register() {
           >
             <MenuItem value="admin">Admin</MenuItem>
             <MenuItem value="user">User</MenuItem>
-          </TextField>
+          </TextField> */}
+          <TextField
+            label="Password"
+            variant="outlined"
+            fullWidth
+            type="password"
+            value={formData?.enrollmentId || ""}
+            onChange={handleChange("enrollmentId")}
+          />
           <TextField
             label="Password"
             variant="outlined"
@@ -138,19 +147,12 @@ function Register() {
             value={formData?.password || ""}
             onChange={handleChange("password")}
           />
-          <TextField
-            label="Confirm Password"
-            variant="outlined"
-            fullWidth
-            type="password"
-            value={formData?.confirmPassword || ""}
-            onChange={handleChange("confirmPassword")}
-          />
 
           <Button
             variant="contained"
             fullWidth
             size="large"
+            type="submit"
             sx={{
               backgroundColor: "#543098",
               color: "white",
